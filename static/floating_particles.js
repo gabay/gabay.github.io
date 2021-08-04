353d2d287298394b65187b411c6e1f60bfec0062
Gabay <roigby@gmail.com>
2021-08-04 09:33:51 +0300

    floating_particles: expose some parameters.

--- static/floating_particles.js
+++ static/floating_particles.js
@@ -2,6 +2,16 @@
 // Global variables
 var canvas = document.querySelector("#scene");
 var ctx = canvas.getContext("2d");
+var minSizeInput = document.querySelector("#min-size");
+var maxSizeInput = document.querySelector("#max-size");
+var spacingInput = document.querySelector("#spacing");
+var rotationInput = document.querySelector("#rotation");
+var accelerationInput = document.querySelector("#acceleration");
+var dragInput = document.querySelector("#drag");
+var shapeCircleInput = document.querySelector("#shape-circle");
+var shapeLineInput = document.querySelector("#shape-line");
+var resetInput = document.querySelector("#reset");
+
 var particles = [];
 var mouse = {x:-999, y:-999};
 var radius = 100;
@@ -13,12 +23,13 @@ var colors = ["#3c3c3c", "#696969", "#969696", "#c3c3c3"];    // Grayscale
 var ww = canvas.width = window.innerWidth;
 var wh = canvas.height = window.innerHeight;
 
-const PARTICLE_DISTANCE = 50;
-const MIN_SIZE = 5, MAX_SIZE = 15;
-const RANDOM_ROTATION = (2 * Math.PI) / 60;
-const INITIAL_SPEED = 20;
-const RANDOM_ACCELERATION = 1;
-const DRAG = 0.005;
+var PARTICLE_DISTANCE = 50;
+var MIN_SIZE = 5, MAX_SIZE = 15;
+var RANDOM_ROTATION = (2 * Math.PI) / 60;
+var INITIAL_SPEED = 20;
+var RANDOM_ACCELERATION = 1;
+var DRAG = 0.005;
+var SHAPE = "circle";
 
 // Particle functionality
 
@@ -66,13 +77,19 @@ class Particle {
     draw() {
         ctx.fillStyle = this.color;
         ctx.beginPath();
-        let offsetX = this.size * Math.cos(this.rotation);
-        let offsetY = this.size * Math.sin(this.rotation);
-        ctx.moveTo(this.x - offsetX, this.y - offsetY);
-        ctx.lineTo(this.x + offsetX, this.y + offsetY);
-        ctx.stroke();
-        // ctx.arc(this.x, this.y, this.size, Math.PI * 2, false);
-        // ctx.fill();
+        if (SHAPE == "circle") {
+            ctx.arc(this.x, this.y, this.size, Math.PI * 2, false);
+            ctx.fill();
+        } else if (SHAPE == "line") {
+            let offsetX = this.size * Math.cos(this.rotation);
+            let offsetY = this.size * Math.sin(this.rotation);
+            ctx.moveTo(this.x - offsetX, this.y - offsetY);
+            ctx.lineTo(this.x + offsetX, this.y + offsetY);
+            ctx.stroke();
+        } else {
+            alert("Unknown shape: " + SHAPE)
+        }
+
     }
 }
 
@@ -110,11 +127,16 @@ function onClick() {
     radius = (radius + 100) % 500;
 }
 
-function initScene(){
+function initScene() {
     ww = canvas.width = window.innerWidth;
     wh = canvas.height = window.innerHeight;
-    console.log("Width: " + ww);
-    console.log("Height: " + wh);
+
+    PARTICLE_DISTANCE = Number(spacingInput.value);
+    MIN_SIZE = Number(minSizeInput.value);
+    MAX_SIZE = Number(maxSizeInput.value);
+    RANDOM_ROTATION = Number(rotationInput.value) * 2 * Math.PI / 360;
+    RANDOM_ACCELERATION = Number(accelerationInput.value);
+    DRAG = Number(dragInput.value);
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -134,9 +156,28 @@ function render(a) {
     }
 };
 
+function setShapeCircle() {
+    SHAPE = "circle";
+    initScene();
+}
+
+function setShapeLine() {
+    SHAPE = "line";
+    initScene();
+}
+
 // Initialization
 
 window.addEventListener("resize", initScene);
+minSizeInput.addEventListener("keyup", initScene);
+maxSizeInput.addEventListener("keyup", initScene);
+spacingInput.addEventListener("keyup", initScene);
+rotationInput.addEventListener("keyup", initScene);
+accelerationInput.addEventListener("keyup", initScene);
+dragInput.addEventListener("keyup", initScene);
+shapeCircleInput.addEventListener("click", setShapeCircle);
+shapeLineInput.addEventListener("click", setShapeLine);
+resetInput.addEventListener("click", function() {setTimeout(initScene, 1);});
 
 canvas.addEventListener("pointerdown", onPointerDown);
 canvas.addEventListener("pointermove", onPointerMove);
