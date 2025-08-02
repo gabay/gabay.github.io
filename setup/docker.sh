fc58cf6a958846c2abe184acf7d1b4029132f6f4
Gabay <roigby@gmail.com>
2025-08-02 14:58:39 +0300

    docker & starship.

--- setup/docker.sh
+++ setup/docker.sh
@@ -9,7 +9,26 @@ echo "Docker: Setup rootless docker..."
 sudo apt-get install -y dbus-user-session
 
 echo "Docker: Add port binding capability..."
-/usr/sbin/setcap CAP_NET_BIND_SERVICE=eip `which rootlesskit` 
+# /usr/sbin/setcap CAP_NET_BIND_SERVICE=ep `which rootlesskit` 
+sudo sh -c "echo 0 > /proc/sys/net/ipv4/ip_unprivileged_port_start"
 
-echo "Docker: Setup rootless..."
+echo "Docker: Setup rootless docker..."
 dockerd-rootless-setuptool.sh install
+sudo loginctl enable-linger
+export DOCKER_HOST=unix:///run/user/1000/docker.sock
+
+echo "Docker: Disable root docker..."
+sudo systemctl disable --now docker.service docker.socket
+sudo rm /var/run/docker.sock
+
+# EXPERIMENTAL: setup bypassns
+# rm -rf /usr/local/go
+# wget https://go.dev/dl/go1.24.5.linux-amd64.tar.gz
+# tar -C /usr/local -xzf go1.24.5.linux-amd64.tar.gz
+# rm https://go.dev/dl/go1.24.5.linux-amd64.tar.gz
+# git clone --depth 1 https://github.com/rootless-containers/bypass4netns.git
+# cd bypass4netns
+# sudo apt install libseccomp-dev
+# make
+# sudo make install
+# cd -
--- setup/starship.sh
+++ setup/starship.sh
@@ -11,3 +11,8 @@ curl -sS https://gabay.github.io/setup/starship.toml -o ~/.config/starship.toml
 echo "Starship: Add to .bashrc..."
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 eval "$(starship init bash)"
+
+echo "Starship: Setting up YADM..."
+sudo apt install yadm
+yadm clone --bootstrap https://github.com/gabay/config.git
+yadm checkout ~
